b84e685b6134ace2564cf8240b7d94c8ccd59db2
Joydeep Lodh <joydeep.lodh@cognizant.com>
2026-06-20 00:35:27 +0200

    Updated file with new changes in app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def init_session_state():
 # General page configuration and initialization
 st.set_page_config(page_title=ui_title, page_icon=ui_icon, layout="wide")
 st.title(ui_title)
-if len(st.session_state.items()) == 0:
+if not st.session_state.items():
     init_session_state()
 
 # Sidebar button to reset session state
